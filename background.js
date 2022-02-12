5791a08823288e9678e974891134e2d2eee3e47f
H-isaac23 <dev.isaac23@gmail.com>
2022-02-12 22:32:28 +0800

    make checkboxes appear and disappear

--- background.js
+++ background.js
@@ -1,7 +1,8 @@
 chrome.tabs.onUpdated.addListener(async () => {
   const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
-
-  if (tab.url.includes("https://classroom.google.com/")) {
+  if (!tab.url) {
+    console.log("URL not updated");
+  } else if (tab.url.includes("https://classroom.google.com/")) {
     chrome.scripting.executeScript({
       target: { tabId: tab.id },
       function: configPage,
@@ -15,6 +16,8 @@ const configPage = () => {
     classList.forEach((item) => {
       const checkbox = document.createElement("INPUT");
       checkbox.setAttribute("type", "checkbox");
+      checkbox.setAttribute("class", "class-checkbox");
+      checkbox.classList.add("class", "hide-checkbox");
       checkbox.style.position = "absolute";
       checkbox.style.margin = "10px 0 0 10px";
       checkbox.style.height = "1.5em";
@@ -22,6 +25,24 @@ const configPage = () => {
       item.appendChild(checkbox);
     });
   };
+
+  const addStyle = () => {
+    const isStylePresent = document.querySelector("style.checkbox-style");
+    //if style is present, return
+    if (isStylePresent) return;
+
+    const checkboxStyle = document.createElement("style");
+    checkboxStyle.setAttribute("class", "checkbox-style");
+    checkboxStyle.textContent = `
+      .hide-checkbox {
+        display: none;
+      }
+    `;
+    document.body.appendChild(checkboxStyle);
+  };
+
+  // add checkbox styles
+  addStyle();
 
   const isCheckboxPresent = document.getElementById("rm-classroom-checkbox");
   if (isCheckboxPresent) {
@@ -39,6 +60,7 @@ const configPage = () => {
       // Use traditional 'for loops' for IE 11
       getClasses();
       observer.disconnect();
+      targetNode.classList.remove("mutation-exists");
     };
 
     // Create an observer instance linked to the callback function
@@ -50,11 +72,18 @@ const configPage = () => {
     return;
   }
 
+  // create main checkbox
   const checkbox = document.createElement("INPUT");
   checkbox.setAttribute("type", "checkbox");
   checkbox.setAttribute("id", "rm-classroom-checkbox");
   checkbox.style.margin = "2em 0 0 2em";
   checkbox.style.height = "1.5em";
   checkbox.style.width = "1.5em";
+  checkbox.addEventListener("change", (e) => {
+    const classCheckboxes = document.querySelectorAll(".class-checkbox");
+    classCheckboxes.forEach((element) => {
+      element.classList.toggle("hide-checkbox");
+    });
+  });
   document.querySelector("ol").parentElement.prepend(checkbox);
 };
